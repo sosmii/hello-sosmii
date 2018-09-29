a8daae33c488207429b65a7952c0ca1c92450376
sosmii <sosmii.seekajob@gmail.com>
2018-09-29 11:39:17 +0900

    closed #2, Cardsコンポーネントの切り出し

---
+++ components/Cards.vue
@@ -0,0 +1,108 @@
+<template>
+  <div
+    :class="{ 'expand': shouldExpand }"
+    class="card"
+    @click="shouldExpand = true"
+  >
+    <div class="card__title">{{ title }}</div>
+    <div 
+      class="card__body" 
+      v-html="body"/>
+    <div class="card__expand-fake-button"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: () => { return null }
+    },
+    body: {
+      type: String,
+      default: () => { return null }
+    },
+    shouldExpand: {
+      type: Boolean,
+      default: () => { return false }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.card {
+  width: 80vw;
+  max-width: 700px;
+  min-height: 100px;
+  max-height: 200px;
+  margin-bottom: 50px;
+  background-color: #FFF;
+  border-radius: 20px;
+  overflow: hidden;
+  transition: all .2s ease-out;
+  box-shadow: 0 2px 43px -4px rgba(0,0,0,.19);
+  text-align: left;
+  cursor: pointer;
+  transition: max-height 0.5s cubic-bezier(0, 1, 0, 1);
+  position: relative;
+}
+.card:hover {
+  box-shadow: 0 2px 5px rgba(0,0,0,.1), 0 1px 2px rgba(0,0,0,.05);
+}
+.card.expand {
+  max-height: 9999px;
+  transition: max-height 1s ease-in-out;
+  cursor: default;
+}
+.card__title {
+  font-size: 24px;
+  font-weight: 700;
+  padding: 15px 25px 7px 25px;
+}
+.card__body {
+  border-top: 1px solid #C5CDD4;
+  padding: 15px 25px 15px 25px;
+  line-height: 1.5em;
+}
+.card__expand-fake-button {
+  background: linear-gradient(to bottom, rgba(255,255,255,.5), rgba(255,255,255,1));
+  position: absolute;
+  width: 100%;
+  height: 40px;
+  bottom: 0;
+}
+.card__expand-fake-button::before {
+  position: absolute;
+  right: 20px;
+  top: 0;
+  margin: auto;
+  content: '';
+  border: 0;
+  border-right: solid 2px #3E5A72;
+  border-bottom: solid 2px #3E5A72;
+  transform: rotate(45deg);
+}
+.card__expand-fake-button::before {
+  width: 20px;
+  height: 20px;
+  bottom: 0;
+}
+.card.expand > .card__expand-fake-button {
+  background: transparent;
+  height: 0;
+}
+.card.expand > .card__expand-fake-button::before,
+.card.expand > .card__expand-fake-button::after {
+  border: 0;
+}
+.card__body >>> .mask {
+  color: transparent;
+  text-shadow: 0 0 13px rgba(0,0,0,0.5);
+}
+.card__body >>> .indent-1 {
+  padding-left: 1em;
+}
+</style>
--- components/CardsFolder.vue
+++ components/CardsFolder.vue
@@ -1,30 +1,25 @@
 <template>
   <div class="cards-folder">
     <h1 id="page-title">{{ pageTitle }}</h1>
-    <div
-      v-for="(card, index) in cards"
-      :class="{ 'expand': card.expand || -1 < expandTarget.indexOf(index) }"
-      :key="card.displayOrder"
-      class="card"
-      @click="addToExpandTarget(index)"
-    >
-      <div class="card__title">{{ card.title }}</div>
-      <div 
-        class="card__body" 
-        v-html="card.body"/>
-      <div class="card__expand-fake-button"/>
-    </div>
+    <Cards 
+      v-for="card in cards" 
+      :key="card.displayOrder" 
+      :title="card.title" 
+      :body="card.body" 
+      :should-expand="card.expand"/>
     <BaseModal v-if="whileLoading"/>
   </div>
 </template>
 
 <script>
 import BaseModal from '@/components/modals/BaseModal'
+import Cards from '~/components/Cards'
 import { setTimeout } from 'timers'
 
 export default {
   components: {
-    BaseModal
+    BaseModal,
+    Cards
   },
   props: {
     jsonData: {
@@ -32,11 +27,6 @@ export default {
       default: () => {
         return null
       }
-    }
-  },
-  data () {
-    return {
-      expandTarget: []
     }
   },
   computed: {
@@ -73,13 +63,6 @@ export default {
           })
         }
       }, 500)
-    },
-    addToExpandTarget (index) {
-      if (this.expandTarget.indexOf(index) > -1) {
-        return
-      }
-
-      this.expandTarget.push(index)
     }
   }
 }
@@ -102,77 +85,5 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
-}
-.card {
-  width: 80vw;
-  max-width: 700px;
-  min-height: 100px;
-  max-height: 200px;
-  margin-bottom: 50px;
-  background-color: #FFF;
-  border-radius: 20px;
-  overflow: hidden;
-  transition: all .2s ease-out;
-  box-shadow: 0 2px 43px -4px rgba(0,0,0,.19);
-  text-align: left;
-  cursor: pointer;
-  transition: max-height 0.5s cubic-bezier(0, 1, 0, 1);
-  position: relative;
-}
-.card:hover {
-  box-shadow: 0 2px 5px rgba(0,0,0,.1), 0 1px 2px rgba(0,0,0,.05);
-}
-.card.expand {
-  max-height: 9999px;
-  transition: max-height 1s ease-in-out;
-  cursor: default;
-}
-.card__title {
-  font-size: 24px;
-  font-weight: 700;
-  padding: 15px 25px 7px 25px;
-}
-.card__body {
-  border-top: 1px solid #C5CDD4;
-  padding: 15px 25px 15px 25px;
-  line-height: 1.5em;
-}
-.card__expand-fake-button {
-  background: linear-gradient(to bottom, rgba(255,255,255,.5), rgba(255,255,255,1));
-  position: absolute;
-  width: 100%;
-  height: 40px;
-  bottom: 0;
-}
-.card__expand-fake-button::before {
-  position: absolute;
-  right: 20px;
-  top: 0;
-  margin: auto;
-  content: '';
-  border: 0;
-  border-right: solid 2px #3E5A72;
-  border-bottom: solid 2px #3E5A72;
-  transform: rotate(45deg);
-}
-.card__expand-fake-button::before {
-  width: 20px;
-  height: 20px;
-  bottom: 0;
-}
-.card.expand > .card__expand-fake-button {
-  background: transparent;
-  height: 0;
-}
-.card.expand > .card__expand-fake-button::before,
-.card.expand > .card__expand-fake-button::after {
-  border: 0;
-}
-.card__body >>> .mask {
-  color: transparent;
-  text-shadow: 0 0 13px rgba(0,0,0,0.5);
-}
-.card__body >>> .indent-1 {
-  padding-left: 1em;
 }
 </style>
