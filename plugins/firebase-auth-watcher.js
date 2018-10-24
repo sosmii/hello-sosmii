d0df511c536b9c22535d794bdbbd41e6522e33a0
sosmii <setsuya.osumi.private@gmail.com>
2018-10-24 17:58:53 +0900

    Vuexfire用にStoreの構造を変更
    コンポーネントで吐かれるエラーはまだ直してない

--- plugins/firebase-auth-watcher.js
+++ plugins/firebase-auth-watcher.js
@@ -1,60 +1,20 @@
-import personalPublic from '~/assets/jsons/personal-public.json';
-import quittingPublic from '~/assets/jsons/quitting-public.json';
-import desirePublic from '~/assets/jsons/desire-public.json';
-import { fireAuth, fireFunc } from '~/plugins/firebase-setting';
+import { fireAuth } from '~/plugins/firebase-setting';
 
-export default ({ store }, inject) => {
+export default ({ store }) => {
   fireAuth.onAuthStateChanged(async user => {
     if (!user) {
-      store.commit('authState/updateLoginState', false);
-      store.commit('authState/updateRegisterState', false);
-      store.commit('authState/updateAuthorizedState', false);
-      store.commit('authState/updateReservationState', false);
+      store.commit('authState/setLoginState', false);
+      store.commit('setGithubId', null);
 
-      updateCardsDataWithAssets(store);
+      store.dispatch('authState/UNBIND_USER_STATE');
+      store.dispatch('authState/resetAllState');
       return;
     }
 
-    const userState = await getUserState();
-    store.commit('authState/updateLoginState', true);
-    store.commit('authState/updateRegisterState', userState.isRegistered);
-    store.commit('authState/updateAuthorizedState', userState.isAuthorized);
-    store.commit('authState/updateReservationState', userState.hasReserved);
+    store.commit('authState/setLoginState', true);
     store.commit('setGithubId', user.providerData[0].uid);
 
-    if (!userState.isAuthorized) {
-      updateCardsDataWithAssets(store);
-      return;
-    }
-
-    updateCardsDataWithRemote(store);
+    const uid = user.uid;
+    store.dispatch('authState/BIND_USER_STATE', { uid: uid });
   });
-};
-
-// TODO: firestoreと直接繋げる
-// 今のままだとSPAなのにuserStateの更新にリロードが要る
-const getUserState = async () => {
-  const referFunc = fireFunc.httpsCallable('getUserState');
-
-  return (await referFunc()).data;
-};
-
-const updateCardsDataWithAssets = store => {
-  store.commit('jsonData/setPersonal', personalPublic);
-  store.commit('jsonData/setQuitting', quittingPublic);
-  store.commit('jsonData/setDesire', desirePublic);
-};
-
-const updateCardsDataWithRemote = async store => {
-  const cardsData = await getCardsDataFromRemote();
-
-  store.commit('jsonData/setPersonal', cardsData.personal);
-  store.commit('jsonData/setQuitting', cardsData.quitting);
-  store.commit('jsonData/setDesire', cardsData.desire);
-};
-
-const getCardsDataFromRemote = async () => {
-  const fetchFunc = fireFunc.httpsCallable('fetchCardsData');
-
-  return (await fetchFunc()).data;
 };
